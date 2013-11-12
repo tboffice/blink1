9161f766ad03db333500ad99b6ad84934a27869b
tboffice <tbofficed@gmail.com>
2013-11-13 01:28:15 +0900

    add comment

--- blink1-serially-turn-on-lump.sh
+++ blink1-serially-turn-on-lump.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
-# determine multiple blink1 devices
+# 複数のblink(1)をUSBハブでつないだ時に、順番にランプがつくようにする
+# 物理的に順番に並べ替えるために作った
+
+# やってみたけど、物理的に順番を変えた時、並べ替えたあと再度実行するとなぜか順番に点いてくれない
+# ということで没
+# と思ったけど後日やったらうまく行った
 
 BLINK1_PATH=$(which blink1-tool)
 COLOR_CODE=("255,0,0" "255,153,0" "255,255,0" "0,255,0" "0,0,255" "51,0,102" "255,0,255")
 
+# all off
 cnt=0
 while :
 do
@@ -15,6 +21,7 @@ do
   cnt=$(($cnt+1))
 done
 
+# rainbow !!!
 cnt=0
 while :
 do
@@ -24,11 +31,10 @@ do
     break
   fi
   
-  echo sorji: $(($cnt%${#COLOR_CODE[@]}))
+  echo soeji: $(($cnt%${#COLOR_CODE[@]}))
   echo COLOR-CODE: ${COLOR_CODE[$(($cnt%${#COLOR_CODE[@]}))]}
 
   cnt=$(($cnt+1))
-#  echo $cnt
 
 done
 
